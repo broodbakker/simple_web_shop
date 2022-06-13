175df1c5cd8d2853e92330ff2e23226159947737
martijn van de ree <mrtjnvanderee@gmail.com>
2022-06-13 10:49:09 +0200

    added function

--- functions/supermario.tsx
+++ functions/supermario.tsx
@@ -4,6 +4,7 @@ exports.handler = async () => {
   console.log("function ran")
 
   const data = { name: "mario", age: 35, job: "plumber" }
+
   return {
     statusCode: 200,
     body: JSON.stringify(data)
